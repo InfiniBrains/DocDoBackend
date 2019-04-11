f1ecf9c8f07770ed519c8e810f5015b93783422b
Alexandre Tolstenko <tolstenko@gmail.com>
2019-04-11 20:15:38 -0300

    token validation is now ok

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 import {
   Count,
   CountSchema,
+  Options,
   Filter,
   repository,
   Where,
@@ -21,7 +22,12 @@ import {UserRepository} from '../repositories';
 let crypto = require('crypto');
 import {HttpErrors} from '@loopback/rest';
 import * as EmailValidator from 'email-validator';
+import {AnyObject} from '@loopback/repository/src/common-types';
+import {Command, NamedParameters} from '@loopback/repository/dist/common-types';
 let jwt = require('jsonwebtoken');
+let nodemailer = require("nodemailer");
+let sgTransport = require('nodemailer-sendgrid-transport');
+
 
 let genRandomString = function(length:number){
   return crypto.randomBytes(Math.ceil(length/2))
@@ -35,11 +41,29 @@ let sha512 = function(password:string|undefined, salt:string){
   return hash.digest('hex');
 };
 
+let emailCredentials = {
+  auth: {
+    api_user: process.env.SENDGRID_USERNAME as string,
+    api_key: process.env.SENDGRID_PASSWORD as string
+  }
+};
+
+let emailClient = nodemailer.createTransport(sgTransport(emailCredentials));
+
 export class UserController {
   constructor(
     @repository(UserRepository)
     public userRepository : UserRepository,
   ) {}
+
+  async mail(from:string, to:string,subject:string,text:string) : Promise<any> {
+    return emailClient.sendMail({
+      from: from,
+      to: to,
+      subject: subject,
+      text: text
+    });
+  }
 
   @post('/users', {
     responses: {
@@ -81,6 +105,14 @@ export class UserController {
     let token = jwt.sign({ email: newuser.email }, newuser.salt, {
       expiresIn: newuser.defaultTokenExpirationTime*60 // expires in 24 hours
     });
+
+    let emailresult = await this.mail(
+      'contact@docdo.com.br',
+      newuser.email,
+      "[InfiniBrains] Validate you account",
+      'Validate your user at DocDo by clicking <a href="https://infinibrains.herokuapp.com/validate/' + token +'">here</a>.'
+    );
+    console.log(emailresult);
 
     // prepare response
     let userCreateResponse = new UserCreateResponse();
@@ -135,6 +167,44 @@ export class UserController {
     @param.query.object('where', getWhereSchemaFor(User)) where?: Where,
   ): Promise<Count> {
     return await this.userRepository.updateAll(user, where);
+  }
+
+  @get('/users/validate/{token}',{
+    responses:{
+      '200': {
+        description: 'Validate user email',
+        content: {'application/json': {
+          schema: {
+            type: 'object',
+            properties: {response: 'string'},
+          }
+        }},
+      },
+    }
+  })
+  async validate(@param.path.string('token') token: string): Promise<AnyObject> { // TODO: improve error handling
+    let email64 = token.split('.')[1];
+    let buff = new Buffer(email64, 'base64');
+    let email = JSON.parse(buff.toString('utf8')).email;
+
+    if(!EmailValidator.validate(email))
+      throw new HttpErrors.BadRequest('Email malformated!');
+
+    let user = await this.userRepository.findOne( {where:{email:email}});
+    if(!user)
+      throw new HttpErrors.NotFound('User not found!');
+
+
+    try {
+      await jwt.verify(token, user.salt);
+    } catch (e) {
+      throw new HttpErrors.ExpectationFailed('The JWT signature doesnt match with payload and secret. Details: ' + e.message);
+    }
+
+    user.validated = true;
+    await this.userRepository.save(user);
+
+    return {response: 'ok'};
   }
 
   @get('/users/{id}', {
--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,3 +1,2 @@
-export * from './access-token.repository';
 export * from './realm.repository';
 export * from './user.repository';
